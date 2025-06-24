caff8fe5fffefe22870a2c1592f2a0678fddd6de
axyz10649 <168225482+axyz10649@users.noreply.github.com>
2025-06-24 15:37:35 +0530

    Update upload.py

--- routes/upload.py
+++ routes/upload.py
@@ -4,6 +4,22 @@ from services.cloudinary_utils import upload_resume_to_cloudinary
 
 router = APIRouter()
 
+@router.post("/upload-resume")
+async def upload_resume(file: UploadFile = File(...)):
+    if not file.filename.lower().endswith(('.pdf', '.docx')):
+        raise HTTPException(status_code=400, detail="Only PDF or DOCX files are allowed.")
+    try:
+        cloudinary_response = upload_resume_to_cloudinary(file.file)
+        return {
+            "fileName": file.filename,
+            "message": "Resume uploaded successfully",
+            "data": cloudinary_response
+        }
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Upload failed: {str(e)}")
+
+
+
 @router.post("/upload-resumes")
 async def upload_resumes(files: List[UploadFile] = File(...)):
     if not files:
